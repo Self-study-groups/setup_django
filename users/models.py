3e69b6467c435366780ebcc92b85c4e9edc37486
UsernameAnda <piccoaldi@gmail.com>
2022-12-10 05:11:44 +0700

    First Commit

--- users/models.py
+++ users/models.py
@@ -1,3 +1,23 @@
 from django.db import models
 
 # Create your models here.
+
+from django.contrib.auth.models import AbstractBaseUser, PermissionsMixin
+
+class Users(AbstractBaseUser,PermissionsMixin):
+    email = models.EmailField(max_length=254, unique=True)
+    name = models.CharField(max_length=255, null=True, blank=True)
+    is_staff = models.BooleanField(default=False)
+    is_superuser = models.BooleanField(default=False)
+    is_active = models.BooleanField(default=True)
+    last_login = models.BooleanField(auto_now=True)
+    date_joined = models.BooleanField(auto_now_add=True)
+
+    #setting field
+    USERNAME_FIELD = 'email'
+    EMAIL_FIELD = 'email'
+    REQUIRED_FIELDS = []
+
+    class Meta:
+        abstract = False
+
